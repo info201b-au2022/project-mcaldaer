9bd051a047a28adb39ff3cb23a508ff776ec675a
JUGKlNG <k875605050@gmail.com>
2022-11-10 02:55:49 -0800

    added barchart

---
+++ source/barchart.R
@@ -0,0 +1,13 @@
+# Bar chart showing most frequent types of force or outcome of (fatal or non fatal) shooting
+
+# Load packages
+library("tidyverse")
+library("ggplot2")
+
+Use_Of_Force <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-mcaldaer/main/data/Use_Of_Force.csv")
+View(Use_Of_Force)
+ 
+ggplot(Use_Of_Force, aes(x = Incident_Type)) + 
+  geom_bar(stat = "count") + 
+  labs(title = "Bar Chart of Incident Types", x = "Types of Force", y = "Count") +
+  coord_flip()
