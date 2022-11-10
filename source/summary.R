ad5dd54968d99f9f422517bfd9d10c9ece016a60
ivyli2022 <114840464+ivyli2022@users.noreply.github.com>
2022-11-09 21:22:42 -0800

    Update summary.R

--- source/summary.R
+++ source/summary.R
@@ -14,11 +14,15 @@ View(SPD_Officer_Involved_Shooting)
 library("tidyverse")
 library("dplyr")
 
-options(timeout = 120)
+options(timeout = 120) 
 
 # Total Number of Interactions
 num_Stops <- nrow(Terry_Stops)
 print(num_Stops)
+num_force <- nrow(Use_Of_Force)
+print(num_force)
+num_shooting <- nrow(SPD_Officer_Involved_Shooting)
+print(num_shooting)
 
 # Interaction Types
 print(unique(Terry_Stops$Stop.Resolution))
@@ -37,6 +41,27 @@ Num_Citation_Infraction <- nrow(subset(Terry_Stops, Terry_Stops$Stop.Resolution 
 print(Num_Citation_Infraction)
 
 # 2. Analyze general demographics in Seattle
+print(unique(Terry_Stops$Precinct))
+print(unique(Use_Of_Force$Precinct))
+
+# Calculate # in West,South,North,Southwest/SouthWest
+Num_West_Stops <- nrow(subset(Terry_Stops, Terry_Stops$Precinct == "West"))
+print(Num_West_Stops)
+Num_South_Stops <- nrow(subset(Terry_Stops, Terry_Stops$Precinct == "South"))
+print(Num_South_Stops)
+Num_North_Stops <- nrow(subset(Terry_Stops, Terry_Stops$Precinct == "North"))
+print(Num_North_Stops)
+Num_Southwest_Stops <- nrow(subset(Terry_Stops, str_detect(Terry_Stops$Precinct, 'Southwest|SouthWest')))
+print(Num_Southwest_Stops)
+
+Num_West_Force <- nrow(subset(Use_Of_Force, Use_Of_Force$Precinct == "West"))
+print(Num_West_Force)
+Num_South_Force <- nrow(subset(Use_Of_Force, Use_Of_Force$Precinct == "South"))
+print(Num_South_Force)
+Num_North_Force <- nrow(subset(Use_Of_Force, Use_Of_Force$Precinct == "North"))
+print(Num_North_Force)
+Num_Southwest_Force <- nrow(subset(Use_Of_Force, Use_Of_Force$Precinct == "Southwest"))
+print(Num_Southwest_Force)
 
 # 3. How are police interactions in Seattle distributed across race and gender? Does it align with the general demographics? Or is there any bias?
 
