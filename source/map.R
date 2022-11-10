b02ecea3b1666325932130b603bdc09d4d6ba306
Madeleine <madeleinerclarke@gmail.com>
2022-11-09 21:11:55 -0800

    started making map visualization

---
+++ source/map.R
@@ -0,0 +1,62 @@
+install.packages("ggmap")
+library(ggplot2)
+library(ggmap)
+library(dplyr)
+
+#load Officer Involved Shooting dataset 
+raw_OIS <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-mcaldaer/main/data/SPD_Officer_Involved_Shooting__OIS__Data.csv")
+View(raw_OIS)
+
+#selecting relevant columns
+OIS <- raw_OIS %>% 
+  filter(City == "Seattle") %>% 
+  select(Longitude, Latitude, Subject.Race, Fatal) %>% 
+  rename(lat = Latitude, long = Longitude, race = Subject.Race)
+View(OIS)
+
+#here I am setting the parameters for the map layer 
+seattle <- qmap("seattle", zoom = 11, maptype = "hybrid")
+seattle
+
+#here I can get a scatterplot of the longitude/latitude points by fatality
+ggplot(data = OIS)+
+  geom_point(mapping = aes(x = long, y = lat, color = Fatal))
+
+#here I can get a scatterplot of the longitude/latitude points by race
+ggplot(data = OIS)+
+  geom_point(mapping = aes(x = long, y = lat, color = race))
+
+#scatterplot on the map, by fatality 
+seattle +
+  geom_point(data=OIS, 
+             aes(x=long, y=lat, 
+                 color = Fatal))
+
+#scatterplot on the map, by fatality 
+seattle +
+  geom_point(data=OIS, 
+             aes(x=long, y=lat, 
+                 color = race))
+
+
+# map <- get_map("seattle")
+# ggmap(map) +
+#   geom_point(
+#     mapping = aes(x = Lat, y = Long), 
+#     color = "blue"
+#   ) 
+
+
+# Lat <- OIS$Latitude
+# Long <- OIS$Longitude
+
+# map <- get_map("seattle")
+# ggmap(map) +
+#   ggplot(data = OIS) +
+#   geom_point(
+#     mapping = aes(x = lat, y = long))
+
+
+
+# register_google(key = "AIzaSyA6IXtNTqQSNjxLG0vcFgct5eHOChp5MNw")
+
