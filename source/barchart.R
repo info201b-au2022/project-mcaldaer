d62aaa913abda91dc5cab24d2ab1693ca84e8dee
JUGKlNG <k875605050@gmail.com>
2022-11-14 19:03:53 -0800

    bar chart description

--- source/barchart.R
+++ source/barchart.R
@@ -5,8 +5,8 @@ library("tidyverse")
 library("ggplot2")
 
 Use_Of_Force <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-mcaldaer/main/data/Use_Of_Force.csv")
- 
-bar_chart <- ggplot(Use_Of_Force, aes(x = Incident_Type)) + 
-  geom_bar(stat = "count") + 
+
+bar_chart <- ggplot(Use_Of_Force, aes(x = Incident_Type)) +
+  geom_bar(stat = "count") +
   labs(title = "Bar Chart of Incident Types", x = "Types of Force", y = "Count") +
   coord_flip()
