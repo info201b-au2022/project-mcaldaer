2fd6aa090a66082b04a65f2eb80878556ea8d3e9
JUGKlNG <k875605050@gmail.com>
2022-11-14 19:36:52 -0800

    bar chart title

--- source/barchart.R
+++ source/barchart.R
@@ -8,5 +8,5 @@ Use_Of_Force <- read.csv("https://raw.githubusercontent.com/info201b-au2022/proj
 
 bar_chart <- ggplot(Use_Of_Force, aes(x = Incident_Type)) +
   geom_bar(stat = "count") +
-  labs(title = "Bar Chart of Incident Types", x = "Types of Force", y = "Count") +
+  labs(title = "Types of Force Used by Seattle Officers", x = "Types of Force", y = "Count") +
   coord_flip()
