b258571b365fa48d2d3bbe250a51f4ec40c8a055
JUGKlNG <k875605050@gmail.com>
2022-12-08 05:49:12 -0800

    summary and findings

--- shiny/tabs/tab_panel_barchart.R
+++ shiny/tabs/tab_panel_barchart.R
@@ -9,15 +9,13 @@ UOF <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-mcald
 
 tab_panel_barchart <-tabPanel(
   "Use of Force",
-  h2("Subject Race by Incident Type"),
+  h2("Subject Race by Incident Types"),
   sidebarLayout(
     sidebarPanel(selectInput("type", label = "Incident Type", 
-                             choices = c("Level 1 - Use of Force", "Level 2 - Use of Force", "Level 3 - Use of Force", "Level 3 - OIS")), 
+                             choices = c("Level 1 - Use of Force", "Level 2 - Use of Force", "Level 3 - Use of Force", "Level 3 - OIS"))
     ), 
     mainPanel(plotlyOutput("barchart"),
-              hr(),
-              p("Summary"))
+              p("This visualization shows the subject race by Use of Force incident types. The incident types involve level 1, force causing transitory pain or the complaint of transitory pain, and level 2, force causing physical injury greater than transitory pain but less than great or substantial bodily harm. Additionally, level 3 is force causing great bodily harm, substantial bodily harm, loss of consciousness, or death, and OIS means that Officer Involved Shooting took place. This barchart is included to compare the distribution of race involved in different types.")
+    )
   )
 )
-
-
--- shiny/tabs/tab_panel_summary.R
+++ shiny/tabs/tab_panel_summary.R
@@ -5,8 +5,25 @@ library(shiny)
 tab_panel_summary <-tabPanel(
     "Summary and Conclusion",
     h3(em("Findings")),
-    p("Three main takeaways"),
-    h3(em("Discussion: Implications & Limitations")), 
+    p("The goal of our project was to examine data from the Seattle Police Department and produce visualizations about the police interactions in Seattle and their correlation with race and geography. 
+      Our research questions were 'What kinds of interactions or violence occur between police and civilians? What are the most common ones?', 'How are police interactions in Seattle distributed across race and gender? Does it align with the general demographics? Or is there any bias?', and 'How are police interactions in Seattle distributed geographically across the city? Does it align with the general demographics? Or is there any bias?'. 
+      From the bar chart that shows the distribution of race involved in different types, we were able to find out that the proportion of black people involved in Use of Force is much larger than that in general demographics. 
+      Moreover, the race that is involved the most in level 3, the highest level of force, was Black or African American. 
+      This gives an idea to our first and second research question. 
+      The answer to the first research question would be level 1 Use of Force which is force that causes transitory pain or the complaint of transitory pain. 
+      With the findings from the bar chart, the second question could also be answered partially. 
+      White and black people are the major races involved in police interactions, and it does not align with the general demographics as the proportion of black people much larger than the general demographics as stated above. 
+      From the pie chart that displays the perceived races of the people that are stopped by police, we were able to find out that people of the perceived race of White is the group that is stopped the most by police. 
+      This pie chart gave us a complete answer to the second question, which was partially answered by the bar chart. 
+      For the White people, the groups seem to correspond, with the group being the largest demographic in Seattle, and also being the group that recieves the most interactions from the police regardless of the type of interaction. 
+      However, it did not align with black people. 
+      29.71% of black people was stopped by police, 36.1% of black people encountered use of force by officers, while they only compose of 7.06% of the population of the city of Seattle. 
+      From our last chart, the map that plots the most extreme form of police violence Officer Involved Shootings, we were able to find out that  a large cluster of shootings occurred in the city center, in Downtown Seattle and the surrounding areas. 
+      We also observed that shootings lacked in North Seattle like Ballard, Fremont, Magnolia and Queen Anne as well as the neighborhood of Madison Park. 
+      These findings gave us answer to our last question. 
+      The distribution of police interactions in Seattle does not align with the general demographics, as there was absence of shootings in some of the wealthier neighborhoods in the city. 
+      "),
+    h3(em("Discussion: Implications & Limitations")),
     p("Consider recycling info in the implications & limitations section"),
     p("Allison's discussion will go here"),
     h3(em("Conclusion")),
@@ -17,4 +34,3 @@ tab_panel_summary <-tabPanel(
       Thank you to our TA, Vivian Yu for support us through this project"),
     h3("References")
 )
-
